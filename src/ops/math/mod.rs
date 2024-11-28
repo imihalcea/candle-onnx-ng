33089dc9c9e310a2de599583d53f99137403a4fc
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-11-28 21:39:27 +0100

    refactor div

--- src/ops/math/mod.rs
+++ src/ops/math/mod.rs
@@ -4,11 +4,13 @@ mod sign;
 mod add;
 mod sub;
 mod mul;
+mod div;
 
 pub(crate) fn register(registry: &mut OnnxOpRegistry) -> Result<(), OnnxOpError>  {
     registry.insert("Sign", Box::new(sign::Sign))?;
     registry.insert("Add", Box::new(add::Add))?;
     registry.insert("Sub", Box::new(sub::Sub))?;
     registry.insert("Mul", Box::new(mul::Mul))?;
+    registry.insert("Div", Box::new(div::Div))?;
     Ok(())
 }
--- tests/ops.rs
+++ tests/ops.rs
@@ -30,45 +30,7 @@ fn test_evaluation_fails_without_defined_graph() -> Result<()> {
 
 
 
-// "Div"
-#[test]
-fn test_div_operation() -> Result<()> {
-    let manual_graph = utils::create_model_proto_with_graph(Some(GraphProto {
-        node: vec![NodeProto {
-            op_type: "Div".to_string(),
-            domain: "".to_string(),
-            attribute: vec![],
-            input: vec![INPUT_X.to_string(), INPUT_Y.to_string()],
-            output: vec![OUTPUT_Z.to_string()],
-            name: "".to_string(),
-            doc_string: "".to_string(),
-        }],
-        name: "".to_string(),
-        initializer: vec![],
-        input: vec![],
-        output: vec![ValueInfoProto {
-            name: OUTPUT_Z.to_string(),
-            doc_string: "".to_string(),
-            r#type: None,
-        }],
-        value_info: vec![],
-        doc_string: "".to_string(),
-        sparse_initializer: vec![],
-        quantization_annotation: vec![],
-    }));
 
-    let mut inputs: HashMap<String, Tensor> = HashMap::new();
-    inputs.insert(INPUT_X.to_string(), Tensor::new(&[2.], &Device::Cpu)?);
-    inputs.insert(INPUT_Y.to_string(), Tensor::new(&[2.], &Device::Cpu)?);
-
-    let eval = simple_eval(&manual_graph, inputs)?;
-    assert_eq!(eval.len(), 1);
-
-    let z = eval.get(OUTPUT_Z).expect("Output 'z' not found");
-    let first = z.to_vec1::<f64>()?[0];
-    assert_eq!(first, 1.0f64);
-    Ok(())
-}
 
 // "Exp"
 #[test]
