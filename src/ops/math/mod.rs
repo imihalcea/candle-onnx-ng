bff0893a9da96997d901d1b7a8fcb5f8377a0a23
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-11-28 21:30:25 +0100

    refactor sub

--- src/ops/math/mod.rs
+++ src/ops/math/mod.rs
@@ -2,9 +2,11 @@ use crate::ops::{OnnxOpError, OnnxOpRegistry};
 
 mod sign;
 mod add;
+mod sub;
 
 pub(crate) fn register(registry: &mut OnnxOpRegistry) -> Result<(), OnnxOpError>  {
     registry.insert("Sign", Box::new(sign::Sign))?;
     registry.insert("Add", Box::new(add::Add))?;
+    registry.insert("Sub", Box::new(sub::Sub))?;
     Ok(())
 }
