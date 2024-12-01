0006ce4df85868a0ad6821aefc05ff03114b1771
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-12-01 20:28:01 +0100

    register AveragePool, MaxPool

--- src/ops/nn/mod.rs
+++ src/ops/nn/mod.rs
@@ -8,5 +8,7 @@ pub(crate) fn register(registry: &mut OnnxOpRegistry) -> Result<(), OnnxOpError>
     registry.insert("LogSoftmax", Box::new(softmax::LogSoftmax))?;
     registry.insert("Softmax", Box::new(softmax::Softmax))?;
     registry.insert("Dropout", Box::new(dropout::Dropout))?; //training not supported
+    registry.insert("AveragePool", Box::new(avgpool::AveragePool))?;
+    registry.insert("MaxPool", Box::new(maxpool::MaxPool))?;
     Ok(())
 }
