75cb9957a2f90228adc9128b1e1cedf3343113c4
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-12-01 20:53:56 +0100

    fix bad naming

--- src/ops/array/mod.rs
+++ src/ops/array/mod.rs
@@ -1,12 +1,14 @@
 use crate::ops::{OnnxOpError, OnnxOpRegistry};
 
 mod flatten;
-mod shape;
+mod reshape;
 mod transpose;
+mod squeeze;
 
 pub(crate) fn register(registry: &mut OnnxOpRegistry) -> Result<(), OnnxOpError> {
-    registry.insert("Reshape", Box::new(shape::Reshape))?;
+    registry.insert("Reshape", Box::new(reshape::Reshape))?;
     registry.insert("Transpose", Box::new(transpose::Transpose))?;
     registry.insert("Flatten", Box::new(flatten::Flatten))?;
+    registry.insert("Squeeze", Box::new(squeeze::Squeeze))?;
     Ok(())
 }
