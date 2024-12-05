74c8065986199746787de87a23809d6c9756b587
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-12-05 21:14:16 +0100

    add test

--- tests/math_clip.rs
+++ tests/math_clip.rs
@@ -5,53 +5,6 @@ use std::collections::HashMap;
 
 pub mod utils;
 
-
-#[test]
-fn test_clip_no_max_provided() -> candle_core::Result<()> {
-    //see https://onnx.ai/onnx/operators/onnx__Clip.html
-    let manual_graph = create_model();
-
-    let mut inputs: HashMap<String, Tensor> = HashMap::new();
-    inputs.insert(
-        "INPUT_X".to_string(),
-        Tensor::new(&[1.0f32, 2.0, 3.0, 4.0, 5.0], &Device::Cpu)?,
-    );
-    inputs.insert("MIN".to_string(), Tensor::new(&[2.0f32], &Device::Cpu)?);
-
-    let eval = simple_eval(&manual_graph, inputs)?;
-
-    assert_eq!(eval.len(), 1);
-
-    let z = eval.get("OUTPUT_Z").expect("Output 'z' not found");
-    assert_eq!(z.to_vec1::<f32>()?, vec![2.0f32, 2.0, 3.0, 4.0, 5.0]);
-
-    Ok(())
-
-}
-
-#[test]
-fn test_clip_no_min_provided() -> candle_core::Result<()> {
-    //see https://onnx.ai/onnx/operators/onnx__Clip.html
-    let manual_graph = create_model();
-
-    let mut inputs: HashMap<String, Tensor> = HashMap::new();
-    inputs.insert(
-        "INPUT_X".to_string(),
-        Tensor::new(&[1.0f32, 2.0, 3.0, 4.0, 5.0], &Device::Cpu)?,
-    );
-    inputs.insert("MAX".to_string(), Tensor::new(&[4.0f32], &Device::Cpu)?);
-
-    let eval = simple_eval(&manual_graph, inputs)?;
-
-    assert_eq!(eval.len(), 1);
-
-    let z = eval.get("OUTPUT_Z").expect("Output 'z' not found");
-    assert_eq!(z.to_vec1::<f32>()?, vec![1.0f32, 2.0, 3.0, 4.0, 4.0]);
-
-    Ok(())
-
-}
-
 #[test]
 fn test_clip_min_max_provided() -> candle_core::Result<()> {
     let manual_graph = create_model();
@@ -167,6 +120,76 @@ fn test_all_values_below_max() -> candle_core::Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn test_clip_max_not_provided() -> candle_core::Result<()> {
+    //see https://onnx.ai/onnx/operators/onnx__Clip.html
+    let manual_graph = create_model();
+
+    let mut inputs: HashMap<String, Tensor> = HashMap::new();
+    inputs.insert(
+        "INPUT_X".to_string(),
+        Tensor::new(&[1.0f32, 2.0, 3.0, 4.0, 5.0], &Device::Cpu)?,
+    );
+    inputs.insert("MIN".to_string(), Tensor::new(&[2.0f32], &Device::Cpu)?);
+
+    let eval = simple_eval(&manual_graph, inputs)?;
+
+    assert_eq!(eval.len(), 1);
+
+    let z = eval.get("OUTPUT_Z").expect("Output 'z' not found");
+    assert_eq!(z.to_vec1::<f32>()?, vec![2.0f32, 2.0, 3.0, 4.0, 5.0]);
+
+    Ok(())
+
+}
+
+#[test]
+fn test_clip_min_not_provided() -> candle_core::Result<()> {
+    //see https://onnx.ai/onnx/operators/onnx__Clip.html
+    let manual_graph = create_model();
+
+    let mut inputs: HashMap<String, Tensor> = HashMap::new();
+    inputs.insert(
+        "INPUT_X".to_string(),
+        Tensor::new(&[1.0f32, 2.0, 3.0, 4.0, 5.0], &Device::Cpu)?,
+    );
+    inputs.insert("MAX".to_string(), Tensor::new(&[4.0f32], &Device::Cpu)?);
+
+    let eval = simple_eval(&manual_graph, inputs)?;
+
+    assert_eq!(eval.len(), 1);
+
+    let z = eval.get("OUTPUT_Z").expect("Output 'z' not found");
+    assert_eq!(z.to_vec1::<f32>()?, vec![1.0f32, 2.0, 3.0, 4.0, 4.0]);
+
+    Ok(())
+
+}
+
+#[test]
+fn test_clip_min_and_max_not_provided() -> candle_core::Result<()> {
+    //see https://onnx.ai/onnx/operators/onnx__Clip.html
+    let manual_graph = create_model();
+
+    let mut inputs: HashMap<String, Tensor> = HashMap::new();
+    inputs.insert(
+        "INPUT_X".to_string(),
+        Tensor::new(&[1.0f32, 2.0, 3.0, 4.0, 5.0], &Device::Cpu)?,
+    );
+
+    let eval = simple_eval(&manual_graph, inputs)?;
+
+    assert_eq!(eval.len(), 1);
+
+    let z = eval.get("OUTPUT_Z").expect("Output 'z' not found");
+    assert_eq!(z.to_vec1::<f32>()?, vec![1.0f32, 2.0, 3.0, 4.0, 5.0]);
+
+    Ok(())
+
+}
+
+
 
 fn create_model() -> ModelProto {
     utils::create_model_proto_with_graph(Some(GraphProto {
