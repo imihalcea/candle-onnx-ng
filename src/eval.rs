3070a00f4a847454fb6b4401c843b0c1a8fc837d
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-12-07 22:41:17 +0100

    refactor range

--- src/eval.rs
+++ src/eval.rs
@@ -114,36 +114,7 @@ fn simple_eval_(
 
         // TODO: Validate node.input for each operator.
         match node.op_type.as_str() {
-            // https://github.com/onnx/onnx/blob/main/docs/Operators.md#Range
-            "Range" => {
-                let start = get(&node.input[0])?;
-                let limit = get(&node.input[1])?;
-                let delta = get(&node.input[2])?;
-
-                macro_rules! arange_step {
-                    ($t: ty) => {
-                        Tensor::arange_step(
-                            start.to_vec0::<$t>()?,
-                            limit.to_vec0::<$t>()?,
-                            delta.to_vec0::<$t>()?,
-                            &Device::Cpu,
-                        )?
-                    };
-                }
-
-                let output = match start.dtype() {
-                    DType::U8 => arange_step!(u8),
-                    DType::U32 => arange_step!(u32),
-                    DType::I64 => arange_step!(i64),
-                    DType::BF16 => arange_step!(f32),
-                    DType::F16 => arange_step!(f32),
-                    DType::F32 => arange_step!(f32),
-                    DType::F64 => arange_step!(f64),
-                };
-
-                values.insert(node.output[0].clone(), output);
-            }
-            // https://github.com/onnx/onnx/blob/main/docs/Operators.md#Greater
+          // https://github.com/onnx/onnx/blob/main/docs/Operators.md#Greater
             "Greater" => {
                 let a = get(&node.input[0])?;
                 let b = get(&node.input[1])?;
