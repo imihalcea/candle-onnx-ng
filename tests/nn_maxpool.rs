edcca00dc6b164ed15d44e95da49e9ca3f9c7176
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-12-09 21:20:39 +0100

    remove warnnings

--- tests/nn_maxpool.rs
+++ tests/nn_maxpool.rs
@@ -1,15 +1,1 @@
-use candle_core::{Device, Tensor};
-use candle_onnx_ng::onnx::attribute_proto::AttributeType;
-use candle_onnx_ng::onnx::{AttributeProto, GraphProto, NodeProto, ValueInfoProto};
-use candle_onnx_ng::simple_eval;
-use std::collections::HashMap;
-
-mod utils;
-
-// MaxPool
-
-#[test]
-fn test_maxpool_1d_default() -> candle_core::Result<()> {
-    //TO DO test example https://github.com/onnx/onnx/blob/main/docs/Operators.md#MaxPool
-    Ok(())
-}
+//TO DO: no tests in original code
